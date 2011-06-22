ba7e2b8493750883014bd42b2cd76f25b28e7a9d
Denys Goff <d.goff@gmx.de>
2011-06-22 20:12:31 +0200

    delete old code

---
+++ Maze/Astar/Script.fsx
@@ -0,0 +1,6 @@
+﻿// Diese Datei ist ein Skript, das mit F# interaktiv ausgeführt werden kann.  
+// Es kann zur Erkundung und zum Testen des Bibliotheksprojekts verwendet werden.
+// Skriptdateien gehören nicht zum Projektbuild.
+
+
+
